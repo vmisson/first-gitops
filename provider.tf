1e3d50733561c9236d21ee9df419ee70a9b67231
Vincent Misson <git@misson.cloud>
2022-09-26 23:47:46 +0200

    First Terraform github action config

--- provider.tf
+++ provider.tf
@@ -6,15 +6,15 @@ terraform {
     }
   }
   backend "azurerm" {
-    subscription_id      = "09675314-6fc3-4b4c-95b3-d62ef3c04c76"
-    resource_group_name  = "admin-tools-rg"
-    storage_account_name = "tfstatevmi"
+    subscription_id      = "16623543-0d38-47d3-82e7-7049f80d7fa0"
+    resource_group_name  = "Infra-Management"
+    storage_account_name = "tfstatevmisson"
     container_name       = "tfstate"
-    key                  = "poc.tfstate"
+    key                  = "gitops.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "09675314-6fc3-4b4c-95b3-d62ef3c04c76"
+  subscription_id = "16623543-0d38-47d3-82e7-7049f80d7fa0"
 }
